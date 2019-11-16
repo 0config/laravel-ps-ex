0f5e0f0a204c2d578dc6cc8ad5689c3d57a593ed
P Singh <singh.prabesh@hotmail.com>
2019-11-16 10:42:03 -0500

    returnJsonError added
    returnJsonError

--- app/Http/Controllers/ControllerExt.php
+++ app/Http/Controllers/ControllerExt.php
@@ -1,46 +1,61 @@
 <?php namespace App\Http\Controllers;
 
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Auth;
-    use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Auth;
 
+class ControllerExt extends Controller
+{
 
-    class ControllerExt extends Controller
+    public function isApiRequest() // TODO add this in gen
+
     {
-        public function PS_qFlashRedirect($message = 'your message here', $classSuffix = 'success', $inRoute = 'home') // changed to home 
-        {
-            session()->flash('message', $message . ' Psx');
-            session()->flash('type', $classSuffix); // OPTIONS bs-class : info|success|danger|warning
-
-            return redirect()->route($inRoute); // you redirect logic here..
-            exit;
-        }
-
-        public function PS_exit($msg='Your Error Message Here')
-        {
-            header("Location: /home/?error=$msg&Psx");
-            exit;
-        }
-
-        public function PS_getResultOrX($id, $skipAuth = false)
-        {
-
-            $classNameOnly =   ( new \ReflectionClass($this))->getShortName() ; 
-            $dynamicModel =  "App\\".substr( $classNameOnly, 0, -10); 
-            $modelName = new  $dynamicModel ;
-            $pramod = $modelName::find($id);
-
-
-            if ($pramod == null) {
-                // do your event logging here
-                $this->PS_exit('record not found_psx');
-            }
-            if ($skipAuth == false) {
-                if (Auth::id() != $pramod->user_id) {
-                    // do your event logging here
-                    $this->PS_exit('not authorized_Psx');
-                }
-            }
-            return $pramod;
-        }
+        return (substr($_SERVER['REQUEST_URI'], 0, 5) == '/api/') ? true : false;
     }
+
+    public function returnJsonError($id = null, $message = 'record not found or not authorized')
+    {
+        header("Content-type:application/json");
+        exit(json_encode(['status' => 'error', 'success' => false, 'id' => $id, 'message' => $message]));
+
+    }
+
+    public function PS_qFlashRedirect($message = 'your message here', $classSuffix = 'success', $inRoute = 'home') // changed to home
+
+    {
+        session()->flash('message', $message . ' Psx');
+        session()->flash('type', $classSuffix); // OPTIONS bs-class : info|success|danger|warning
+
+        return redirect()->route($inRoute); // you redirect logic here..
+        exit;
+    }
+
+    public function PS_getResultOrX($id, $skipAuth = false)
+    {
+
+        $classNameOnly = (new \ReflectionClass($this))->getShortName();
+        if (substr($classNameOnly, 0, 3) == 'Api') {
+            $classNameOnly = substr($classNameOnly, 3, strlen($classNameOnly));
+        }
+
+        $dynamicModel = "App\\" . substr($classNameOnly, 0, -10);
+        $modelName = new $dynamicModel;
+        $result = $modelName::find($id);
+
+        if ($result == null) {
+            // do your event logging here
+            if (self::isApiRequest() == true) {
+                // TODO extract this to a method with custom error messages
+                $this->returnJsonError($id, 'record not found or not authorized ..');
+            }
+            $this->returnJsonError($id, 'record not found or not authorized ...'); // this may not be required
+        }
+
+        if ($skipAuth == false) {
+            if (Auth::id() != $result->user_id) {
+                // do your event logging here
+                $this->returnJsonError($id, 'record not found or not authorized ....');
+            }
+        }
+
+        return $result;
+    }
+}
